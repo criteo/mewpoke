245689498d68fa7427c10ef237f2693524e35b88
Romain GÉRARD <r.gerard@criteo.com>
2018-08-14 17:23:20 +0200

    Attempt to isolate more coubhase cluster failure between them

--- src/main/java/com/criteo/nosql/mewpoke/couchbase/CouchbaseMonitor.java
+++ src/main/java/com/criteo/nosql/mewpoke/couchbase/CouchbaseMonitor.java
@@ -42,7 +42,6 @@ import java.util.stream.Collectors;
 
 public class CouchbaseMonitor implements AutoCloseable {
     private static Logger logger = LoggerFactory.getLogger(CouchbaseMonitor.class);
-    private static AtomicReference<CouchbaseEnvironment> couchbaseEnv = new AtomicReference<>(null);
 
     private final String serviceName;
 
@@ -135,7 +134,7 @@ public class CouchbaseMonitor implements AutoCloseable {
         CouchbaseCluster client = null;
         Bucket bucket = null;
         try {
-            final CouchbaseEnvironment env = couchbaseEnv.updateAndGet(e -> e == null ? DefaultCouchbaseEnvironment.builder().retryStrategy(FailFastRetryStrategy.INSTANCE).build() : e);
+            final CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder().retryStrategy(FailFastRetryStrategy.INSTANCE).build();
             final int httpDirectPort = env.bootstrapHttpDirectPort();
             client = CouchbaseCluster.create(env, endPoints.stream().map(e -> e.getHostString()).collect(Collectors.toList()));
             bucket = client.openBucket(bucketName, bucketpassword);
